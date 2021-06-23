73b610db72a0e463be1f594fe36271d25e23daa7
Jon McKinley <jmckinley@xenatech.com>
2021-06-23 07:34:15 -0600

    few changes for driver interactions with CHP

--- Web Site/FSP.Web.4.5/MyScripts/AssistEntry/AssistScript.js
+++ Web Site/FSP.Web.4.5/MyScripts/AssistEntry/AssistScript.js
@@ -1,7 +1,7 @@
 var DriverName = "";
 var TruckID = "";
 var TruckStatus = "";
-var ServiceLocation = "http://38.124.164.213:9007/";
+var ServiceLocation = "http://38.124.164.211:9007/";
 
 var ClientStatus = "LoggedOff";
 
